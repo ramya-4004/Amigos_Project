42f9b4d128539fe44991e9f5e3fd6cac2599f0d2
S P Ramya Pratyusha <spr.pratyusha2001@gmail.com>
2020-10-10 10:12:33 +0530

    Changes in the frontend

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,7 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,16 +16,15 @@ public class Controller {
     public Button adminButton;
     public Button registerButton;
 
-
     public void clickListener(ActionEvent actionEvent){
         Stage stage = (Stage) loginButton.getScene().getWindow();
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("Login/login.fxml"));
+            root = FXMLLoader.load(getClass().getResource("Login/userPage.fxml"));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        stage.setScene(new Scene(root, 300,300));
+        stage.setScene(new Scene(root, 600,500));
     }
 
     public void adminListener(ActionEvent actionEvent){
--- src/sample/Login/Login.java
+++ src/sample/Login/Login.java
@@ -1,4 +1,51 @@
 package sample.Login;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
+import java.lang.*;
+
+
 public class Login {
+
+    public Button login;
+    public Button logout;
+
+    public void loginListener(ActionEvent event) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root1 = fxmlLoader.load();
+            Stage stage = (Stage)login.getScene().getWindow();
+            stage.setTitle("User Page");
+            stage.setScene(new Scene(root1, 300, 400));
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void logoutActionListener(ActionEvent event){
+        try {
+
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("login.fxml"));
+            Parent root2 = fxmlLoader.load();
+            Stage stage = (Stage)logout.getScene().getWindow();
+            Scene scene = new Scene(root2, 300, 300);
+            stage.setScene(scene);
+            stage.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
